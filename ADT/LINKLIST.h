b8f13213e54a0f38ebafe9bd1ac13fb0cc35816b
Jalor <jalor@JalordeMacBook-Pro.local>
2020-09-04 16:53:53 +0800

    feat:完成链表

--- ADT/LINKLIST.h
+++ ADT/LINKLIST.h
@@ -62,4 +62,93 @@ void LinkList<DataType>::PrintList() {
     cout<<endl;
 }
 
+/*输出链表长度*/
+template <typename DataType>
+int LinkList<DataType>::Length() {
+    Node<DataType> * p = first -> next;
+    int cnt = 0;
+    while (p) {
+        p = p -> next;
+        cnt ++;
+    }
+    return cnt;
+}
+
+/*按位查找*/
+template <typename DataType>
+DataType LinkList<DataType>::Get(int i){
+    Node<DataType> *p = first -> next;
+    int pos = 1;
+    while(p && pos < i){
+        p = p -> next;
+        pos ++;
+    }
+    if(!p) throw "illage index";
+    return p -> data;
+}
+
+/*按值查找*/
+template <typename DataType>
+int LinkList<DataType>::Locate(DataType x){
+    Node<DataType> * p = first -> next;
+    int pos = 1;
+    while (p) {
+        if(p -> data == x){
+            return pos;
+        }
+        p = p -> next;
+        pos ++;
+    }
+    return -1;
+}
+
+/*往第i个位置插入值x*/
+template <typename DataType>
+void LinkList<DataType>::Insert(int i, DataType x){
+    Node<DataType> *p = first -> next;
+    int pos = 1;
+    
+    while (p && pos < i - 1) {
+        pos++;
+        p = p -> next;
+    }
+    
+    Node<DataType> *newNode = new Node<DataType>;
+    newNode -> data = x;
+    
+    newNode -> next = p -> next;
+    p -> next = newNode;
+}
+
+/*删除第i个位置的值x，并展示出来*/
+template <typename DataType>
+DataType LinkList<DataType>::Delete(int i){
+    DataType x;
+    Node<DataType> *p = first -> next;
+    
+    int pos = 1;
+    
+    while (p && pos < i - 1) {
+        p = p -> next;
+        pos ++;
+    }
+    
+    Node<DataType> *del = p -> next;
+    p -> next = del -> next;
+    x = del -> data;
+    delete del;
+    return x;
+}
+
+
+/*判断是否为空 0 = 为空， 其他数字 = 不为空*/
+template <typename DataType>
+int LinkList<DataType>::Empty()
+{
+    Node<DataType> *p = first -> next;
+    if(p){
+        return 1;
+    }
+    return 0;
+}
 #endif /* LINKLIST_h */
